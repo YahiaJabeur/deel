a227953888e184a80c13bb37e25f1923286c924b
Yahia Jabeur <yahiajabeur@gmail.com>
2024-02-11 21:17:16 +0100

    fix: payslip pdf file location

--- src/payslips.ts
+++ src/payslips.ts
@@ -5,120 +5,120 @@ export const payslipsArray: Payslip[] = [
     id: '123456',
     fromDate: '2024-01-01',
     toDate: '2024-01-31',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123457',
     fromDate: '2024-02-01',
     toDate: '2024-02-29',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123458',
     fromDate: '2024-03-01',
     toDate: '2024-03-31',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123459',
     fromDate: '2024-04-01',
     toDate: '2024-04-30',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123460',
     fromDate: '2024-05-01',
     toDate: '2024-05-31',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123461',
     fromDate: '2024-06-01',
     toDate: '2024-06-30',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123462',
     fromDate: '2024-07-01',
     toDate: '2024-07-31',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123463',
     fromDate: '2024-08-01',
     toDate: '2024-08-31',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123464',
     fromDate: '2024-09-01',
     toDate: '2024-09-30',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123465',
     fromDate: '2024-10-01',
     toDate: '2024-10-31',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123466',
     fromDate: '2024-11-01',
     toDate: '2024-11-30',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123467',
     fromDate: '2024-12-01',
     toDate: '2024-12-31',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123468',
     fromDate: '2025-01-01',
     toDate: '2025-01-31',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123469',
     fromDate: '2025-02-01',
     toDate: '2025-02-28',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123470',
     fromDate: '2025-03-01',
     toDate: '2025-03-31',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123471',
     fromDate: '2025-04-01',
     toDate: '2025-04-30',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123472',
     fromDate: '2025-05-01',
     toDate: '2025-05-31',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123473',
     fromDate: '2025-06-01',
     toDate: '2025-06-30',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123474',
     fromDate: '2025-07-01',
     toDate: '2025-07-31',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   },
   {
     id: '123475',
     fromDate: '2025-08-01',
     toDate: '2025-08-31',
-    fileUrl: './payslip.pdf'
+    fileUrl: './assets/payslip.pdf'
   }
 ]
